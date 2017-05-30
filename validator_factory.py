e04f13e29c7e951467b8212496cfd01634da27ce
kiki <charles@skeletalstudios.com>
2017-05-30 17:07:26 -0400

    Added basic logging / log clearing to ValidatorFactory.

--- validator_factory.py
+++ validator_factory.py
@@ -2,8 +2,9 @@ import imp
 from imp import reload
 import os, sys
 
-from scene_check.validators import base_validator as bv
-reload( bv )
+from scene_check.validators import base_validator
+reload( base_validator )
+from scene_check.validators.base_validator import BaseValidator as bv
 
 import bpy
 
@@ -14,11 +15,14 @@ class ValidatorFactoryException(Exception):
 
 ## ----------------------------------------------------------------------
 class ValidatorFactory(object):
-	def __init__(self):
+	def __init__(self, clear=True):
 		self.modulePath = os.sep.join( [__file__.rpartition( os.sep )[0], 'validators'] )
 		self.modules = [ x.partition('.')[0] for x in os.listdir(self.modulePath) if x.endswith('.py') 
 					and not x.count('__init') 
 					and not x.count('base_validator') ]
+
+		if clear:
+			self.clear_log()
 
 	## ----------------------------------------------------------------------
 
@@ -27,6 +31,14 @@ class ValidatorFactory(object):
 
 	def __setitem__(self, key, value):
 		raise ValueError("ValidatorFactory does not allow the setting of values through brackets.")
+
+	## ----------------------------------------------------------------------
+	def clear_log(self):
+		if not bv.log_name in bpy.data.texts:
+			log = bpy.data.texts.new( bv.log_name )
+		else:
+			log = bpy.data.texts[bv.log_name]
+			log.clear()
 
 	## ----------------------------------------------------------------------
 	def get_class( self, name ):
