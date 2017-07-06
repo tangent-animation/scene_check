a4eabb6d9e30d8cbc1d2d4a2465a43d0830c4a79
kiki <charles@skeletalstudios.com>
2017-07-06 12:04:56 -0400

    RigArmatureNCR.py auto-fix updated, and fixed bug in selection code.

--- validators/RigArmatureNCR.py
+++ validators/RigArmatureNCR.py
@@ -20,17 +20,29 @@ class RigArmatureNCR(BaseValidator):
 			if arm.name.startswith('ncr.'):
 				if not arm.hide or not arm.hide_select or not arm.hide_render:
 					self.error(
-						ob=arm,
+						ob=arm.name,
 						type='ncr_hide',
+						select_func='object',
 						message='NCR Armature {} is not properly hidden.'.format( arm.name )
 					)
 
+					fix_code = (
+						'item = bpy.data.objects["{}"]\n'
+						'item.hide = item.hide_select = item.hide_render = True'
+					).format( arm.name )
 
-	def automatic_fix_hook( self ):
-		for error in self.errors:
-			if error.type == 'ncr_hide':
-				error.ob.hide        = True
-				error.ob.hide_select = True
-				error.ob.hide_render = True
-				print( "+ Automatically fixed bendy for control bone {}::{}.".format(error.ob.name, error.subob) )
+					self.auto_fix_last_error(
+						fix_code,
+						message=('Set proper visibility on NCR Armature "{}".'
+								.format( arm.name )
+						)
+					)
+
+	# def automatic_fix_hook( self ):
+	# 	for error in self.errors:
+	# 		if error.type == 'ncr_hide':
+	# 			error.ob.hide        = True
+	# 			error.ob.hide_select = True
+	# 			error.ob.hide_render = True
+	# 			print( "+ Automatically fixed bendy for control bone {}::{}.".format(error.ob.name, error.subob) )
 
