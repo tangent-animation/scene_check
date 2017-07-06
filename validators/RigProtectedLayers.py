dbb80625d8396dd92bb644c6bf0ec844a1d9aed9
kiki <charles@skeletalstudios.com>
2017-07-06 14:11:47 -0400

    RigProtectedLayers.py auto-fix enabled.

--- validators/RigProtectedLayers.py
+++ validators/RigProtectedLayers.py
@@ -4,20 +4,47 @@ import bpy
 
 from scene_check.validators.base_validator import BaseValidator
 
-class RigProtectedLayers(BaseValidator):
+class RigProtectedLayers( BaseValidator ):
 	automatic_fix = True
 
 	def __init__(self):
-		super(RigProtectedLayers, self).__init__()
+		super( RigProtectedLayers, self ).__init__()
 
 	def process_hook( self ):
-		pass
-
-	def automatic_fix_hook(self):
 		armatures = self.get_objects( type='ARMATURE' )
 		if not len(armatures):
 			return
 
 		for arm in armatures:
-			arm.data.layers_protected = ([False] * 32)
-		print( "+ Automatically hid protected layers ({} armatures).".format(len(armatures)) )
+			total = sum( x for x in arm.data.layers_protected )
+			if not total == 32:
+				self.error(
+					ob=arm.name,
+					select_func='object',
+					type='ARMATURE:PROTECTED LAYERS',
+					message='Armature "{}" does not have all its protected layers set.'
+							.format(arm.name)
+				)
+
+				fix_code = (
+					'bpy.data.objects["{}"].data.layers_protected = ( [True] * 32 )'
+					.format( arm.name )
+				)
+
+				self.auto_fix_last_error(
+					fix_code,
+					message=(
+						'Set protected layers on armature "{}".'
+						.format( arm.name )
+					)
+				)
+
+	# def automatic_fix_hook(self):
+	# 	armatures = self.get_objects( type='ARMATURE' )
+	# 	if not len(armatures):
+	# 		return
+
+	# 	for arm in armatures:
+	# 		arm.data.layers_protected = ([False] * 32)
+
+	# 	print( "+ Automatically hid protected layers ({} armatures).".format(len(armatures)) )
