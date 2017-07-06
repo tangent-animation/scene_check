025731ff6785f20808d2d4f17bf7d0585e119315
kiki <charles@skeletalstudios.com>
2017-07-06 15:11:02 -0400

    SceneConstraints.py auto-fix enabled.

--- validators/SceneConstraints.py
+++ validators/SceneConstraints.py
@@ -31,12 +31,31 @@ class SceneConstraints(BaseValidator):
 
 		self.constraints_group_name = 'grp.constraints_' + self.scene.name
 
-		if not self.constraints_group_name in self.scene.objects:
+		group_fix_code = (
+			'if not "{group_name}" in bpy.data.objects:\n'
+			'\tgroup = bpy.data.objects.new( "{group_name}", None )\n'
+			'\tbpy.context.scene.objects.link( group )\n'
+			'else:\n'
+			'\tgroup = bpy.data.objects["{group_name}"]\n'
+			'group.rotation_mode = "XYZ"\n'
+			'group.lock_location = ( True, True, True )\n'
+			'group.lock_rotation = ( True, True, True )\n'
+			'group.lock_scale    = ( True, True, True )\n'
+			'group.hide_select   = True\n'
+			'group.hide_render   = True\n'
+		).format( group_name=self.constraints_group_name )
+
+		if (not self.constraints_group_name in self.scene.objects) and len(self.constraint_objects):
 			self.error(
 				ob=self.scene.name,
 				type="SCENE:CONSTRAINTS GROUP",
 				message=( 'Constraints group object "{}" is missing.'
 					.format( self.constraints_group_name ) )
+			)
+
+			self.auto_fix_last_error(
+				group_fix_code,
+				message=( 'Add constraints group "{}".'.format(self.constraints_group_name) )
 			)
 
 		for item in self.constraint_objects:
@@ -52,31 +71,44 @@ class SceneConstraints(BaseValidator):
 						.format( item.name, self.constraints_group_name ) )
 				)
 
-	def automatic_fix_hook( self ):
-		## loop twice to ensure the scene constraint group
-		for error in self.errors:
-			if error.type == 'SCENE:CONSTRAINTS GROUP':
-				group = bpy.data.objects.new( self.constraints_group_name, None )
-				self.scene.objects.link( group )
+				## not sure when the user will run one or the other of the fixes,
+				## so ensure the group exists at all times
+				fix_code = group_fix_code + (
+					'constraint = bpy.context.scene.objects["{}"]\n'
+					'constraint.parent = group'
+				).format( item.name )
 
-				group.rotation_mode = 'XYZ'
-				group.lock_location = ( True, True, True )
-				group.lock_rotation = ( True, True, True )
-				group.lock_scale    = ( True, True, True )
-				group.hide_select   = True
-				group.hide_render   = True
+				self.auto_fix_last_error(
+					fix_code,
+					message=( 'Parent constraint "{}" to constraints group "{}".'
+						.format(item.name, self.constraints_group_name) )
+				)
 
-				print( '+ Added constraint group "{}".'.format(group.name) )
+	# def automatic_fix_hook( self ):
+	# 	## loop twice to ensure the scene constraint group
+	# 	for error in self.errors:
+	# 		if error.type == 'SCENE:CONSTRAINTS GROUP':
+	# 			group = bpy.data.objects.new( self.constraints_group_name, None )
+	# 			self.scene.objects.link( group )
 
-		group = bpy.data.objects[ self.constraints_group_name ]
+	# 			group.rotation_mode = 'XYZ'
+	# 			group.lock_location = ( True, True, True )
+	# 			group.lock_rotation = ( True, True, True )
+	# 			group.lock_scale    = ( True, True, True )
+	# 			group.hide_select   = True
+	# 			group.hide_render   = True
 
-		for error in self.errors:
-			if error.type == 'SCENE:CONSTRAINTS':
-				constraint = bpy.data.objects[ error.ob ]
-				constraint.parent = group
+	# 			print( '+ Added constraint group "{}".'.format(group.name) )
 
-				print( '+ Reparented constraint "{}" to "{}".'
-						.format( self.constraints_group_name, error.ob) )
+	# 	group = bpy.data.objects[ self.constraints_group_name ]
+
+	# 	for error in self.errors:
+	# 		if error.type == 'SCENE:CONSTRAINTS':
+	# 			constraint = bpy.data.objects[ error.ob ]
+	# 			constraint.parent = group
+
+	# 			print( '+ Reparented constraint "{}" to "{}".'
+	# 					.format( self.constraints_group_name, error.ob) )
 
 
 
