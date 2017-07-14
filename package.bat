b970c9601db122d8726c2e61a802b6eb99081220
kiki <charles@skeletalstudios.com>
2017-07-14 16:51:54 -0400

    New package.

--- package.bat
+++ package.bat
@@ -1,6 +1,6 @@
 @echo off
 pushd ..
-zip -r9 "scene_check.zip" scene_check\*.py scene_check\validators\*.py
+zip -r9 "scene_check.zip" scene_check\*.py scene_check\validators\*.py scene_check\schemes\*.scheme
 popd
 move ..\scene_check.zip .
 
